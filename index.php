aae42317750cb4112bfaac8cd4da33fccfdf8917
jafaralierkinov6-sys <jafaralierkinov6@gmail.com>
2026-05-06 10:11:38 +0500

    first back end code

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    echo "
+   <h1>O'quvchilar royhati</h1>
+    <table style='border:1px solid black;'>
+  <tr style='border:1px solid black;'>
+    <th>ISMLAR</th>
+    <th>BAHOLAR</th>
+    <th>FIZIKA</th>
+    <th>INGLIZ TILI</th>
+    <th>BIOLOGIYA</th>
+    <th>MATEMATIKA</th>
+    <th>RUS TILI</th>
+  </tr>
+
+  <tr style=' text-align:center;'>
+    <td>JAFARALI</td>
+    <td>5</td>
+    <td>4</td>
+    <td>3</td>
+    <td>5</td>
+    <td>4</td>
+    <td>5</td>
+  </tr>
+
+  </table>  
+
+" ;
